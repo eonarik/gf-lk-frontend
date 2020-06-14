b6818898472f68bd40eda7419e324f5f66c7110f
Oleg.Kosnyrev <eonarik@mail.ru>
2020-06-14 06:19:45 +0300

    fix social auth buttons

--- src/components/SocialAuth/SocialAuth.vue
+++ src/components/SocialAuth/SocialAuth.vue
@@ -4,16 +4,24 @@
       Или войти через соц. сети
     </label>
     <div class="auth-providers">
-      <a href="?hauth_action=login&amp;provider=Google">
+      <a target="_blank"
+        :href="`${$router.history.current.fullPath}?hauth_action=login&amp;provider=Google`"
+      >
         <i class="fa fa-google-plus"></i>
       </a>
-      <a href="?hauth_action=login&amp;provider=Vkontakte">
+      <a target="_blank"
+        :href="`${$router.history.current.fullPath}?hauth_action=login&amp;provider=Vkontakte`"
+      >
         <i class="fa fa-vk"></i>
       </a>
-      <a href="?hauth_action=login&amp;provider=Instagram">
+      <a target="_blank"
+        :href="`${$router.history.current.fullPath}?hauth_action=login&amp;provider=Instagram`"
+      >
         <i class="fa fa-instagram"></i>
       </a>
-      <a href="?hauth_action=login&amp;provider=Facebook">
+      <a target="_blank"
+        :href="`${$router.history.current.fullPath}?hauth_action=login&amp;provider=Facebook`"
+      >
         <i class="fa fa-facebook"></i>
       </a>
     </div>
